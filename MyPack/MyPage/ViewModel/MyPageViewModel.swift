601639e05dd18db02a5e256ed7333c276694180f
hanni66 <haeunkim0807@gmail.com>
2023-04-17 17:30:31 +0900

    [#38] mypage-view-ui 파일생성

---
+++ MyPack/MyPage/ViewModel/MyPageViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageViewModel.swift
+//  MyPack
+//
+//  Created by 김하은 on 2023/04/16.
+//
+
+import UIKit
+
+class MyPageViewModel {
+//    private let mypageService: MyPageServiceProtocol
+//    weak var mypageCoordinator: MyPageCoordinatorProtocol?
+
+//    init(mypageService: MyPageServiceProtocol, mypageCoordinator: MyPageCoordinatorImpl) {
+//        self.mypageCoordinator = mypageCoordinator
+//        self.mypageService = mypageService
+//    }
+}
